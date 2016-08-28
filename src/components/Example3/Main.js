c174259815f1b4cb26e31e904c579452258c3235
jevgeni <zeka.rum@gmail.com>
2016-08-28 16:37:20 +0300

    some progress with 3rd example

--- src/components/Example3/Main.js
+++ src/components/Example3/Main.js
@@ -1,16 +1,36 @@
 /**
- * Created by zekar on 8/28/2016.
+ * Created by zekar on 8/26/2016.
  */
+require('normalize.css/normalize.css');
+require('styles/App.css');
 
 import createShader from '../../helpers/createShader';
 import createProgram from '../../helpers/createProgram';
 
 import React from 'react';
 
-import vertexShaderSource from './shaders/vertexShader.glsl';
-import fragmentShaderSource from './shaders/fragmentShader.glsl';
+const vertexShaderSource = `
+attribute vec2 vertPosition;
+attribute vec3 vertColor;
+varying vec3 fragColor1;
+uniform mat4 mWorld;
+uniform mat4 mView;
+uniform mat4 mProj;
+void main() {
+  fragColor1 = vertColor;
+  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 0.0, 1.0);
+}
+`;
 
-class Example3 extends React.Component {
+const fragmentShaderSource = `
+precision mediump float;
+varying vec3 fragColor1;
+void main() {
+  gl_FragColor = vec4(fragColor1, 1.0);
+}
+`;
+
+class Main extends React.Component {
   componentDidMount() {
     this.updateCanvas();
   }
@@ -19,6 +39,7 @@ class Example3 extends React.Component {
     const canvas = this.refs.canvas;
     const gl = canvas.getContext('webgl');
 
+    // //gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.74, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -26,61 +47,71 @@ class Example3 extends React.Component {
     var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
     var program = createProgram(gl, vertexShader, fragmentShader);
 
-    const triangleVertices =
-      [
-        0.0, 0.5, 0.0,    1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.0,  0.7, 0.0, 1.0,
-        0.5, -0.5, 0.0,   0.1, 1.0, 0.6
-      ];
+    const triangleVertices = [
+    // X,    Y,   R, G, B
+      0.0, 0.5, 0.0, 1.0, 1.0, 0.0,
+      -0.5, -0.5, 0.0, 1.0, 0.0, 1.0,
+      0.5, -0.5, 0.0, 0.3, 0.5, 0.1
+    ];
 
     const triangleVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    // gl.STATIC_DRAW - sending data from CPU to GPU only once
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
 
     const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-    gl.vertexAttribPointer(positionAttribLocation,
-      3,
+
+    gl.vertexAttribPointer(
+      positionAttribLocation,
+      3, // number of elements per attribute
       gl.FLOAT,
       gl.FALSE,
-      6 * Float32Array.BYTES_PER_ELEMENT,
-      0);
-    gl.vertexAttribPointer(colorAttribLocation,
-      3,
+      6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex.
+      0// Offset from the beginning of single vertex to this attirbute
+    );
+
+    gl.vertexAttribPointer(
+      colorAttribLocation,
+      3, // number of elements per attribute
       gl.FLOAT,
       gl.FALSE,
-      6 * Float32Array.BYTES_PER_ELEMENT,
-      3 * Float32Array.BYTES_PER_ELEMENT);
+      6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex.
+      3 * Float32Array.BYTES_PER_ELEMENT// Offset from the beginning of single vertex to this attirbute
+    );
 
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(colorAttribLocation);
 
+    /*
+      Main render loop
+     */
+
     gl.useProgram(program);
 
-    let matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-    let matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-    let matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+    const matWorldUniformLocation = gl.getUniformLocation(program, "mWorld");
+    const matViewUniformLocation = gl.getUniformLocation(program, "mView");
+    const matProjUniformLocation = gl.getUniformLocation(program, "mProj");
 
-    let worldMatrix = new Float32Array(16);
-    let viewMatrix = new Float32Array(16);
-    let projMatrix = new Float32Array(16);
+    const matWorld = new Float32Array(16);
+    const matView = new Float32Array(16);
+    const matProj = new Float32Array(16);
+    mat4.identity(matWorld);
+    mat4.identity(matView);
+    mat4.identity(matProj);
 
-    mat4.identity(worldMatrix);
-    mat4.identity(viewMatrix);
-    mat4.identity(projMatrix);
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, matWorld);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, matView);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, matProj);
 
-    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-
-    gl.drawArrays(gl.TRIANLGES, 0, 3);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);  // gl.TRIANGLES - used practically 99% of all times
   }
 
   render() {
     return (<div>
-      <canvas ref="canvas" width={300} height={300}></canvas>
-    </div>)
+        <canvas ref="canvas" width={300} height={300}></canvas>
+      </div>);
   }
 }
 
-export default Example3;
+export default Main;
